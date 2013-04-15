8dba2e9e292cce7c333aa1dd0f4736c65a84995c
Hans Loken <hans@ebamse.net>
2013-04-15 12:40:36 +0200

    Started on using Castle Windor in MVC
    Started (not finished) on implementing Castle Windsor as Controller (and
    dependencies) factory. Using this for a starting point: // As
    inspired(or copied from):
    http://docs.castleproject.org/Windsor.Windsor-tutorial-ASP-NET-MVC-3-application-To-be-Seen.ashx

---
+++ Northwind.WebClientMvc4/Installers/ControllersInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Northwind.WebClientMvc4.Installers
+{
+    // As inspired(or copied from): http://docs.castleproject.org/Windsor.Windsor-tutorial-ASP-NET-MVC-3-application-To-be-Seen.ashx
+    public class ControllersInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly()
+                                .BasedOn<IController>()
+                                .LifestyleTransient());
+        }
+    }
+}
