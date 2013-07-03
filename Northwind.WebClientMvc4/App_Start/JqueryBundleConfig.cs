6bfd1e5f3097800ba2715faacb4f6210b63e91ff
Hans Loken <hans@ebamse.net>
2013-07-03 15:06:09 +0200

    Created MVC bundle for jQuery

---
+++ Northwind.WebClientMvc4/App_Start/JqueryBundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+using Northwind.WebClientMvc4;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(JqueryBundleConfig), "RegisterBundles")]
+
+namespace Northwind.WebClientMvc4
+{
+    public class JqueryBundleConfig
+    {
+        public static void RegisterBundles()
+        {
+            // Add @Scripts.Render("~/bundles/jquery") before bootstrap or angular in your _Layout.cshtml view
+            // When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
+            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery*"));
+        } 
+    }
+}
